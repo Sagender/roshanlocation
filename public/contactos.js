91a852c232e853a6d5104fe35f97807792705883
Andy <andrea.reyes0098@gmail.com>
2021-07-17 00:40:58 -0500

    details
    (finish by Andrea)

--- public/contactos.js
+++ public/contactos.js
@@ -1,155 +1,43 @@
-var camposFormulario = {
-    nombre: '',
-    telefono: '',
-    correo: '',
-    mensaje: ''
-}
+$(document).ready(function(){
+ 
 
+    $('#btnSend').click(function(){
 
-function leerTextoNombre(evento) {
-    camposFormulario.nombre = evento.target.value;
-    console.table(camposFormulario);
-}
+        var errores = ''; 
 
-function leerTextoTelefono(evento) {
-    camposFormulario.telefono = evento.target.value;
-    console.table(camposFormulario);
-}
+         //Validando Nombre ====================================
+         if( $('#names').val() == '' ){
+             errores += '<p>Escriba un nombre</p>';
+             $('#names').css("border-bottom-color", "#F14B4B")
+         }else{
+            $('#names').css("border-bottom-color", "#d1d1d1")
+         }
+         //Validando Correo ====================================
+         if( $('#email').val() == '' ){
+            errores += '<p>Ingrese un correo</p>';
+            $('#email').css("border-bottom-color", "#F14B4B")
+        }else{
+            $('#email').css("border-bottom-color", "#d1d1d1")
+         }
+        //Validando Mensaje ====================================
+        if( $('#mensaje').val() == '' ){
+            errores += '<p>Escriba un mensaje</p>';
+            $('#mensaje').css("border-bottom-color", "#F14B4B")
+        }else{
+            $('#mensaje').css("border-bottom-color", "#d1d1d1")
+         }
 
-function leerTextoCorreo(evento) {
-    camposFormulario.correo = evento.target.value;
-    console.table(camposFormulario);
-}
+        //Enviando Mensaje ====================================
+        if( errores == '' == false){
+            var mensajeModal = '<div class="modal_wrap">'+
+                                    '<div class="mensaje_modal">'+
+                                        '<h3>Errores encontrados</h3>'+
+                                        errores+
+                                        '<span id="btnClose">Cerrar</span>'+
+                                    '</div>'+
+                                '</div>'
 
-function leerTextoMensaje(evento) {
-    camposFormulario.mensaje = evento.target.value;
-    console.table(camposFormulario);
-}
-
-function leerTexto(evento) {
-    camposFormulario[evento.target.id] = evento.target.value;
-    console.table(camposFormulario);
-}
-
-
-function mostrarMensajeError(mensaje) {
-
-    const bloqueError = document.createElement('p');
-
-    bloqueError.textContent = mensaje;
-
-    bloqueError.classList.add('mensajeError');
-
-    formulario.appendChild(bloqueError);
-
-
-    setTimeout(function() {
-        bloqueError.remove();
-    }, 5000);
-}
-
-function mostrarMensajeOk(mensaje) {
-
-    const bloqueOk = document.createElement('p');
-
-    bloqueOk.textContent = mensaje;
-
-    bloqueOk.classList.add('mensajeOk');
-
-    formulario.appendChild(bloqueOk);
-
-
-    setTimeout(function() {
-        bloqueOk.remove();
-    }, 5000);
-}
-
-function mostrarMensaje(mensaje, bandera) {
-
-    const bloque = document.createElement('p');
-
-    bloque.textContent = mensaje;
-
-    if (bandera === 'Correcto') {
-        bloque.classList.add('mensajeOk');
-    } else {
-        bloque.classList.add('mensajeError');
-    }
-    formulario.appendChild(bloque);
-
-    setTimeout(function() {
-        bloque.remove();
-    }, 5000);
-}
-
-function validarFormulario(evento) {
-    evento.preventDefault();
-
-    if (camposFormulario.nombre === '' || camposFormulario.telefono === '' || camposFormulario.correo === '' || camposFormulario.mensaje === '') {
-
-        mostrarMensaje("Todos los campos son obligatorios", 'Error');
-
-        return;
-    }
-
-    mostrarMensaje("Información enviada correctamente", 'Correcto');
-}
-
-const Nombre = document.getElementById('nombre');
-const telefono = document.getElementById('telefono');
-const correo = document.querySelector('#correo');
-const mensaje = document.querySelector('#mensaje');
-const formulario = document.querySelector('.formulario-contacto');
-
-
-Nombre.addEventListener('input', leerTexto);
-telefono.addEventListener('input', leerTexto);
-correo.addEventListener('input', leerTexto);
-mensaje.addEventListener('input', leerTexto);
-
-
-
-formulario.addEventListener('submit', validarFormulario);
-
-
-function imprimirEnConsola() {
-
-}
-
-
-window.addEventListener('load', imprimirEnConsola);
-
-window.addEventListener('scroll', function() {
-
-});
-
-
-window.addEventListener('DOMContentLoaded', function() {
-
-});
-window.addEventListener('DOMContentLoaded', function() {
-
-});
-
-
-const botonEnviar = document.querySelector('#botonEnviar');
-console.log(botonEnviar);
-botonEnviar.addEventListener('click', function(miEvento) {
-    console.log(miEvento);
-    miEvento.preventDefault();
-    console.log("Enviando datos del formulario");
-});
-
-
-
-console.clear();
-const nombre = document.querySelector('#nombre');
-
-nombre.addEventListener('change', function() {
-    console.log("Escribiendo...");
-});
-
-nombre.addEventListener('input', function(evento) {
-    console.log(evento);
-    console.log("Escribiendo...");
-});S
+            $('body').append(mensajeModal);
+        }
+        });
+    });
--- public/noche_tg.js
+++
@@ -1,9 +0,0 @@
-function switchSheet() {
-    let theme = document.getElementById("theme");
-
-    if (theme.getAttribute("href") == "stylestrucosguias.css") {
-        theme.href = "noche_tg.css";
-    } else {
-        theme.href = "stylestrucosguias.css";
-    }
-}
