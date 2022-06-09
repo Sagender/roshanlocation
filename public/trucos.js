198078cf20c08a73fec0de20a7358fd6ea19a833
sagender <sagender1623@gmail.com>
2022-06-09 01:02:06 -0500

    modificación de la página

--- public/trucos.js
+++
@@ -1,9 +0,0 @@
-function switchSheet() {
-    let theme = document.getElementById("theme");
-  
-    if (theme.getAttribute("href") == "stylestrucosguias.css") {
-      theme.href = "noche.css";
-    } else {
-      theme.href = "stylestrucosguias.css";
-    }
-    }
